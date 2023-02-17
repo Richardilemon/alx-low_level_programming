b2c0d1e018c4498243e5fb0c407bdeed74c36540
Richardilemon <richardiupeju16@gmail.com>
2023-02-17 15:24:38 -0800

    Never give up

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,8 @@
 #include <unistd.h>
 
 /**
- *main - Prints out the last part of a quote in the standard error.
- *Return: 1 if success.
+ * main - Prints out the last part of a quote in the standard error.
+ * Return: 1 if success.
  */
 
 int main(void)
