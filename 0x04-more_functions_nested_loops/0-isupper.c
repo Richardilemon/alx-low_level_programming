33c61cbf7f0495da0f52dc655ed998726cf5256e
Richardilemon <richardiupeju16@gmail.com>
2023-02-24 12:08:03 -0800

    zero

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,16 +4,22 @@
  *
  * _isupper - checks whether a given string contains an uppercase character
  *
- * @str: The string to check
+ * @c: The string to check
  *
  * Return: 1 if the string contains an uppercase character, 0 otherwise
+ *
  */
-int _isupper(char *str) 
+
+int _isupper(int c)
 {
-	for (int i = 0; str[i] != '\0'; i++) {
-		if (str[i] >= 'A' && str[i] <= 'Z') {
-			return 1;
-		}
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+
 	}
-	return 0;
+	else
+	{
+		return (0);
+	}
+
 }
