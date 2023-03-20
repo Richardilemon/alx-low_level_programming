41d95ceff86ae986b5101bc7ed4cec63c2f01184
Richardilemon <richardiupeju16@gmail.com>
2023-03-20 08:30:14 -0700

    Object-like Macro

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef MYHEADER_H
+#define MYHEADER_H
+#include <stdio.h>
+
+/**
+ * @file myheader.h
+ * @brief Header file that defines the SIZE macro
+ */
+
+#define SIZE 1024
+
+#endif /* MYHEADER_H */
+
