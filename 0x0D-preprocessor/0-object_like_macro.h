b02dfc5c3f6c7e8641defd54fe014f18babf275e
Richardilemon <richardiupeju16@gmail.com>
2023-03-20 08:33:05 -0700

    Object-like Macro

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,5 +1,5 @@
-#ifndef MYHEADER_H
-#define MYHEADER_H
+#include "0-object_like_macro.h"
+#include "0-object_like_macro.h"
 #include <stdio.h>
 
 /**
@@ -9,5 +9,4 @@
 
 #define SIZE 1024
 
-#endif /* MYHEADER_H */
-
+#endif
