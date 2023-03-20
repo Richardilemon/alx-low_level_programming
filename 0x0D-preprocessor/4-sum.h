0bdb4fa69d369053d0a37e6d06e93b4d3c72dcd9
Richardilemon <richardiupeju16@gmail.com>
2023-03-20 09:29:35 -0700

    SUM

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef MYHEADER_4
+#define MYHEADER_4
+
+/**
+ * SUM - Computes the sum of two numbers
+ * @x: First number to add
+ * @y: Second number to add
+ *
+ * Return: The sum of the two numbers
+ */
+#define SUM(x, y) ((x) + (y))
+
+#endif
