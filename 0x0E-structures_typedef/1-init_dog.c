e0c5434cb4b28c632def26fffac33823a8e0aa30
Richardilemon <richardiupeju16@gmail.com>
2023-03-21 14:44:13 -0700

    love yourself

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,7 +12,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    if (d != NULL) // make sure d is not a null pointer
+    if (d != NULL)
     {
         d->name = name;
         d->age = age;
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,11 +9,17 @@
  *
  * Description: This struct represents a dog with a name, age, and owner.
  */
-typedef struct dog
+struct dog
+
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
