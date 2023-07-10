ef16af316e714363d707a79fcfaec945efc000c6
richardilemon <richardiupeju16@gmail.com>
2023-07-10 15:22:15 -0700

    One

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,53 +1,38 @@
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
- * @filename: The name of the file to read.
- * @letters: The number of letters to read and print.
+ * read_textfile - function that reads and prints out the output.
+ * @filename: File to be read and printed.
+ * @letters: number of letters to be printed.
  *
- * Return: The actual number of letters read and printed, or 0 on failure.
+ * Return: number of letters to be read and printed.
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t total = 0;
-	ssize_t bytes_read;
-	ssize_t bytes_written;
-	char buffer[1024];
+	int fd;
+	ssize_t nr, nw;
+	char *buf;
 
 	if (filename == NULL)
-		return (0);
-
-	FILE *file = fopen(filename, "r");
-	if (file == NULL)
-		return (0);
-
-	while (letters > 0 && (bytes_read = fread(buffer, sizeof(char), sizeof(buffer), file)) > 0)
 	{
-		if (bytes_read <= letters)
-		{
-			bytes_written = fwrite(buffer, sizeof(char), bytes_read, stdout);
-			if (bytes_written != bytes_read)
-			{
-				fclose(file);
-				return (0);
-			}
-			total += bytes_written;
-			letters -= bytes_written;
-		}
-		else
-		{
-			bytes_written = fwrite(buffer, sizeof(char), letters, stdout);
-			if (bytes_written != letters)
-			{
-				fclose(file);
-				return (0);
-			}
-			total += bytes_written;
-			letters = 0;
-		}
+		return (0);
 	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	buf = malloc(sizeof(char) * letters + 1);
+	nr = read(fd, buf, letters);
+	nw = write(STDOUT_FILENO, buf, nr);
 
-	fclose(file);
-	return (total);
+	if (nr == -1 || nw == -1)
+	{
+		return (0);
+	}
+	close(fd);
+	free(buf);
+	return (nw);
 }
 
